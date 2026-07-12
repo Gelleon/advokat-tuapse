4713aef4953c72e2c7df0c39a519e18b4162598a
Gelleon <pallermo72@gmail.com>
2026-07-12 13:57:37 +0300

    Fix CORS configuration

--- server/index.ts
+++ server/index.ts
@@ -14,9 +14,19 @@ const app = express();
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = process.env.FRONTEND_URL 
-  ? [process.env.FRONTEND_URL, 'http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000'] 
-  : ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000'];
+const allowedOrigins = [
+  'http://advokat-tuapse.ru',
+  'https://advokat-tuapse.ru',
+  'http://www.advokat-tuapse.ru',
+  'https://www.advokat-tuapse.ru',
+  'http://localhost:5174',
+  'http://localhost:5173',
+  'http://localhost:3000'
+];
+
+if (process.env.FRONTEND_URL) {
+  allowedOrigins.push(process.env.FRONTEND_URL);
+}
 
 app.use(cors({
   origin: function (origin, callback) {
